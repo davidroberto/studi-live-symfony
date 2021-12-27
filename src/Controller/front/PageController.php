15ca391b66c5f6ca15cdffe72918335a5c900edb
David Robert <contact.davidrobert@gmail.com>
2021-12-27 19:04:16 +0100

    mailer

--- src/Controller/front/PageController.php
+++ src/Controller/front/PageController.php
@@ -4,7 +4,12 @@
 namespace App\Controller\front;
 
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Message;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
@@ -21,5 +26,53 @@ class PageController extends AbstractController
             'message' => $message
         ]);
     }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(Request $request, MailerInterface $mailer)
+    {
+        $postRequest = $request->request;
+
+        $firstNameUser = $postRequest->get('firstName');
+        $lastNameUser = $postRequest->get('lastName');
+        $emailUser = $postRequest->get('email');
+        $messageUser = $postRequest->get('message');
+
+        $emailToAdmin = new TemplatedEmail();
+        $emailToAdmin->from('contact.davidrobert@gmail.com')
+            ->to('contact.davidrobert@gmail.com')
+            ->subject($emailUser.' vous a envoyé un message !')
+            ->htmlTemplate('emails/contact.html.twig')
+            ->context([
+                'firstName' => $firstNameUser,
+                'lastName' => $lastNameUser,
+                'emailUser' => $emailUser,
+                'message' => $messageUser
+            ])
+        ;
+
+        $autoEmailToUser = new Email();
+        $autoEmailToUser->from('contact.davidrobert@gmail.com')
+            ->to($emailUser)
+            ->subject('Votre demande a été reçue')
+            ->text('Nous allons traiter votre demande après digestion du fois gras')
+        ;
+
+
+        $mailer->send($emailToAdmin);
+        $mailer->send($autoEmailToUser);
+
+
+
+
+        // au clic sur le bouton "envoyer", on va récuprer les données POST
+        // avec les données de POST (du formulaire), on envoie un mail à l'administrateur
+
+        return $this->render("contact.html.twig");
+
+    }
+
+
 
 }
